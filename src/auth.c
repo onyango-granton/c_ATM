f5f7f97c0f4b99fbdbd7c72a0a9091dd76947d72
gonyango <grantononyango@gmail.com>
2024-10-02 08:15:40 +0300

    add: windows support, login menu and get password func

--- src/auth.c
+++ src/auth.c
@@ -1,50 +1,80 @@
-#include <termios.h>
 #include "header.h"
 
-char *USERS = "./data/users.txt";
+const char *USERS = "../data/users.txt";
 
 void loginMenu(char a[50], char pass[50])
 {
-    struct termios oflags, nflags;
+    int i = 0;
+    char ch;
 
     system("clear");
     printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Login:");
     scanf("%s", a);
 
-    // disabling echo
-    tcgetattr(fileno(stdin), &oflags);
-    nflags = oflags;
-    nflags.c_lflag &= ~ECHO;
-    nflags.c_lflag |= ECHONL;
-
-    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
-    {
-        perror("tcsetattr");
-        return exit(1);
-    }
     printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
+    
+    while (1) {
+        ch = getch();
+        
+        if (ch == 13 || ch == 10) { // Enter key
+            pass[i] = '\0';
+            break;
+        } else if (ch == 127 || ch == 8) { // Backspace
+            if (i > 0) {
+                i--;
+                printf("\b \b");
+            }
+        } else {
+            pass[i++] = ch;
+            printf("*");
+        }
+    }
+}
+
+void registerMenu(char a[50], char pass[50])
+{
+    FILE *fp;
+    struct User u;
+    int id = 0;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Registration:");
+    scanf("%s", a);
+
+    printf("\n\n\n\n\n\t\t\t\tEnter the password:");
     scanf("%s", pass);
 
-    // restore terminal
-    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    fp = fopen(USERS, "a+");
+
+    if (fp == NULL)
     {
-        perror("tcsetattr");
-        return exit(1);
+        printf("Error opening file\n");
+        exit(1);
     }
-};
+
+    while (fscanf(fp, "%d %s %s", &u.id, u.name, u.password) != EOF)
+    {
+        id = u.id;
+    }
+
+    fprintf(fp, "%d %s %s\n", id + 1, a, pass);
+    fclose(fp);
+
+    printf("\n\nUser registered successfully!\n");
+}
 
 const char *getPassword(struct User u)
 {
     FILE *fp;
     struct User userChecker;
 
-    if ((fp = fopen("./data/users.txt", "r")) == NULL)
+    if ((fp = fopen("../data/users.txt", "r")) == NULL)
     {
         printf("Error! opening file");
         exit(1);
     }
 
-    while (fscanf(fp, "%s %s", userChecker.name, userChecker.password) != EOF)
+    while (fscanf(fp, "%d %s %s", &userChecker.id, userChecker.name, userChecker.password) != EOF)
     {
         if (strcmp(userChecker.name, u.name) == 0)
         {
@@ -57,3 +87,34 @@ const char *getPassword(struct User u)
     fclose(fp);
     return "no user found";
 }
+
+
+#ifdef _WIN32
+int getch(void)
+{
+    return _getch();
+}
+
+void clearScreen(void)
+{
+    system("cls");
+}
+#else
+int getch(void)
+{
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+}
+
+void clearScreen(void)
+{
+    system("clear");
+}
+#endif
