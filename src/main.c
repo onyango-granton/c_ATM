35e759dd762ecd937b44db89fe61b0e3ea8bb6b0
gonyango <grantononyango@gmail.com>
2024-10-02 08:17:13 +0300

    first program interface containing main menu and initialize menu

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ void mainMenu(struct User u)
     printf("\n\t\t-->> Feel free to choose one of the options below <<--\n");
     printf("\n\t\t[1]- Create a new account\n");
     printf("\n\t\t[2]- Update account information\n");
-    printf("\n\t\t[3]- Check accounts\n");
+    printf("\n\t\t[3]- Check account details\n");
     printf("\n\t\t[4]- Check list of owned account\n");
     printf("\n\t\t[5]- Make Transaction\n");
     printf("\n\t\t[6]- Remove existing account\n");
@@ -22,27 +22,22 @@ void mainMenu(struct User u)
         createNewAcc(u);
         break;
     case 2:
-        // student TODO : add your **Update account information** function
-        // here
+        updateAccount(u);
         break;
     case 3:
-        // student TODO : add your **Check the details of existing accounts** function
-        // here
+        checkAccountDetails(u);
         break;
     case 4:
         checkAllAccounts(u);
         break;
     case 5:
-        // student TODO : add your **Make transaction** function
-        // here
+        makeTransaction(u);
         break;
     case 6:
-        // student TODO : add your **Remove existing account** function
-        // here
+        removeAccount(u);
         break;
     case 7:
-        // student TODO : add your **Transfer owner** function
-        // here
+        transferOwner(u);
         break;
     case 8:
         exit(1);
@@ -50,7 +45,7 @@ void mainMenu(struct User u)
     default:
         printf("Invalid operation!\n");
     }
-};
+}
 
 void initMenu(struct User *u)
 {
@@ -72,17 +67,15 @@ void initMenu(struct User *u)
             if (strcmp(u->password, getPassword(*u)) == 0)
             {
                 printf("\n\nPassword Match!");
+                r = 1;
             }
             else
             {
                 printf("\nWrong password!! or User Name\n");
-                exit(1);
             }
-            r = 1;
             break;
         case 2:
-            // student TODO : add your **Registration** function
-            // here
+            registerMenu(u->name, u->password);
             r = 1;
             break;
         case 3:
@@ -92,12 +85,11 @@ void initMenu(struct User *u)
             printf("Insert a valid operation!\n");
         }
     }
-};
+}
 
 int main()
 {
     struct User u;
-    
     initMenu(&u);
     mainMenu(u);
     return 0;
