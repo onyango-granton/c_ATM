a5d2944492ead46ae79307ab85e8aa034d15f0c3
gonyango <grantononyango@gmail.com>
2024-10-02 08:16:22 +0300

    file that links other project files

--- src/header.h
+++ src/header.h
@@ -1,13 +1,22 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#endif
 
 struct Date
 {
     int month, day, year;
 };
 
-// all fields for each record of an account
 struct Record
 {
     int id;
@@ -29,12 +38,29 @@ struct User
     char password[50];
 };
 
-// authentication functions
+// Authentication functions
 void loginMenu(char a[50], char pass[50]);
 void registerMenu(char a[50], char pass[50]);
 const char *getPassword(struct User u);
 
-// system function
+// System functions
 void createNewAcc(struct User u);
+void updateAccount(struct User u);
+void checkAccountDetails(struct User u);
+void makeTransaction(struct User u);
+void removeAccount(struct User u);
+void transferOwner(struct User u);
 void mainMenu(struct User u);
 void checkAllAccounts(struct User u);
+
+// Utility functions
+int getch(void);
+void saveAccountToFile(FILE *ptr, struct User u, struct Record r);
+int getAccountFromFile(FILE *ptr, char name[50], struct Record *r);
+void success(struct User u);
+void stayOrReturn(int notGood, void f(struct User u), struct User u);
+
+// Cross-platform clear screen function
+void clearScreen(void);
+
+#endif // HEADER_H
